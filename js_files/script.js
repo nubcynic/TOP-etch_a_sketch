5afa9e600ea21ea34b1ff506663cf427d1203963
nubcynic <201962971+nubcynic@users.noreply.github.com>
2025-06-09 11:52:03 +0545

    feat: update bg color of 16x16 grid boxes
    All grid boxes have same class: grids, hence looped through all the boxes
    and added event listener so that the bg color changes upon mouse hover

--- js_files/script.js
+++ js_files/script.js
@@ -1,7 +1,17 @@
 const container = document.querySelector("#container");
 const gridSize = 16;
+// Create square grids based on grid size
 for (let i = 0; i < (gridSize * gridSize); i++) {
     const gridItem = document.createElement("div");
     gridItem.classList.add("grids");
     container.appendChild(gridItem);
 };
+// Reference to the above created grids as nodelist
+const grids = document.querySelectorAll(".grids");
+
+// Upon mouseover, each grid changes its bg color
+grids.forEach(grid => {
+    grid.addEventListener("mouseover", () => {
+        grid.style.backgroundColor = "blue";
+    });
+});
